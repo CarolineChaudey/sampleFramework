2c9eb8ff25b805478029a157e72a4ca939246e95
CarolineChaudey <etudiant@iMac-de-etudiant-28.local>
2018-01-17 15:08:41 +0100

    begin

---
+++ SampleFramework/Pair.swift
@@ -0,0 +1,15 @@
+//
+//  Pair.swift
+//  SampleFramework
+//
+//  Created by etudiant on 17/01/2018.
+//  Copyright © 2018 esgi. All rights reserved.
+//
+
+import Foundation
+
+extension Int {
+    public var isPair: Bool {
+        return self % 2 == 0
+    }
+}
